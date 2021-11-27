a818a1e0b4a45cf0406ecb02b9735104aec5180d
Alan Santos <alan.santos.ex.contato@gmail.com>
2021-11-27 04:50:57 -0300

    starter commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/global.scss'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Hello World Alan! Let's code!</h1>
+      <p>We made it. Changed</p>
+      <p>Added SASS/SCSS</p>
+    </>
+  )
+}
