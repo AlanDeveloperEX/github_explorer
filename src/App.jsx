a97fc76a313dbd71ad299916eea7fd7e71776637
Alan Santos <alan.santos.ex.contato@gmail.com>
2022-09-20 07:33:19 -0300

    refactor: edit some details

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
-import './styles/global.scss'
-import RepositoryList from './components/RepositoryList'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./styles/global.scss";
+
+import RepositoryList from "./components/RepositoryList";
 
 export default function App() {
-
   return (
     <>
       <CssBaseline />
 
-      <RepositoryList/>
+      <RepositoryList />
     </>
-  )
+  );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,17 @@
-import React, { useEffect } from 'react'
-import { Container , Grid, List, ListItemButton, ListItemText, Typography, Link, CircularProgress, Box } from '@material-ui/core';
-import LinkIcon from '@material-ui/icons/Link';
-import { BoxStyled } from './styles.ts';
+import React, { useEffect } from "react";
+import {
+  Container,
+  Grid,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+  Link,
+  CircularProgress,
+  Box,
+} from "@material-ui/core";
+import LinkIcon from "@material-ui/icons/Link";
+import { BoxStyled } from "./styles.ts";
 
 const RepositoryList = () => {
   const [repoInfo, setRepoInfo] = React.useState([]);
@@ -10,26 +20,36 @@ const RepositoryList = () => {
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
 
-    console.log(event.target.innerText, index)
+    console.log(event.target.innerText, index);
   };
 
   useEffect(() => {
-    fetch('https://api.github.com/users/AlanDeveloperEX/repos')
-      .then(response => response.json())
-      .then(data => setRepoInfo(data))
-  }, [])
+    fetch("https://api.github.com/users/AlanDeveloperEX/repos")
+      .then((response) => response.json())
+      .then((data) => setRepoInfo(data));
+  }, []);
 
   return (
     <Container>
       <Grid mt={2} container spacing={2} justify="center" display="flex">
         <Grid container item xs={12} display="flex" justifyContent="center">
-          <Typography variant="h3" component="div" gutterBottom color="common.white">
+          <Typography
+            variant="h3"
+            component="div"
+            gutterBottom
+            color="common.white"
+          >
             -- Git Repository List --
           </Typography>
         </Grid>
         <Grid container item xs={12} display="flex" justifyContent="center">
-          <Typography variant="h5" component="div" gutterBottom color="common.white">
-            Index Clicked: {(selectedIndex > -1) ? selectedIndex : 'Anyone'}
+          <Typography
+            variant="h5"
+            component="div"
+            gutterBottom
+            color="common.white"
+          >
+            Index Item Clicked: {selectedIndex > -1 ? selectedIndex : "Anyone"}
           </Typography>
         </Grid>
       </Grid>
@@ -37,64 +57,72 @@ const RepositoryList = () => {
         <Grid item xs={12}>
           <BoxStyled
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '100%'
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
             }}
           >
-            <List component="nav" aria-label="secondary mailbox folder" sx={{
-              flex: '1',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'column-reverse',
-              '&& .Mui-selected, && .Mui-selected:hover': {
-                bgcolor: '#050a308f',
-                '&, & .MuiListItemIcon-root': {
-                  color: '#fff',
+            <List
+              component="nav"
+              aria-label="secondary mailbox folder"
+              sx={{
+                flex: "1",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column-reverse",
+                "&& .Mui-selected, && .Mui-selected:hover": {
+                  bgcolor: "#050a308f",
+                  "&, & .MuiListItemIcon-root": {
+                    color: "#fff",
+                  },
                 },
-              },
-              '& .MuiListItemButton-root:hover': {
-                bgcolor: '#050a308f',
-                '&, & .MuiListItemIcon-root': {
-                  color: '#fff',
+                "& .MuiListItemButton-root:hover": {
+                  bgcolor: "#050a308f",
+                  "&, & .MuiListItemIcon-root": {
+                    color: "#fff",
+                  },
                 },
-              },
-            }}>
-              {(repoInfo.length > 0) ? repoInfo.map((repository, key) => 
-                <ListItemButton
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-around',
-                    width: '100%'
-                  }}
-                  key={repository.id}
-                  selected={selectedIndex === key}
-                  alignItems="center"
-                  onClick={(event) => handleListItemClick(event, key)}
-                >
-                  <ListItemText primary={repository.name} />
-                  <Link 
-                    href={repository.html_url} 
-                    color="inherit" 
-                    underline="none" 
-                    rel="noreferrer" 
-                    target="_blank"
-                    display="flex"
+              }}
+            >
+              {repoInfo.length > 0 ? (
+                repoInfo.map((repository, key) => (
+                  <ListItemButton
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-around",
+                      width: "100%",
+                    }}
+                    key={repository.id}
+                    selected={selectedIndex === key}
                     alignItems="center"
+                    onClick={(event) => handleListItemClick(event, key)}
                   >
-                    <LinkIcon/>
-                  </Link >
-                </ListItemButton>
-              ) : <CircularProgress />}
+                    <ListItemText primary={repository.name} />
+                    <Link
+                      href={repository.html_url}
+                      color="inherit"
+                      underline="none"
+                      rel="noreferrer"
+                      target="_blank"
+                      display="flex"
+                      alignItems="center"
+                    >
+                      <LinkIcon />
+                    </Link>
+                  </ListItemButton>
+                ))
+              ) : (
+                <CircularProgress />
+              )}
             </List>
           </BoxStyled>
         </Grid>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default RepositoryList
+export default RepositoryList;
